29518b3b9fd91546d0b4bafacbe4eb870086dab4
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-03-03 13:43:13 +0400

    compilation and resource bundle

--- DozorClient/src/main/java/com/dozor/langs/LocaleBundle.java
+++ DozorClient/src/main/java/com/dozor/langs/LocaleBundle.java
@@ -1,15 +1,29 @@
 package com.dozor.langs;
 
+import com.joconner.i18n.Utf8ResourceBundleControl;
+
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
  * @author IGOR-K
  */
 public class LocaleBundle {
+    private final static ResourceBundle.Control UTF_8_CONTROL = new Utf8ResourceBundleControl();
+    private final static ResourceBundle SELF;
 
-    private final static ResourceBundle SELF
-            = ResourceBundle.getBundle("com.dozor.langs.locales.Words", new Locale("ru", "RU"));
+    static {
+        ResourceBundle bundle;
+        try {
+            bundle = ResourceBundle.getBundle("com.dozor.langs.locales.Words", new Locale("ru", "RU"));
+        } catch (MissingResourceException e) {
+            e.printStackTrace();
+            bundle = ResourceBundle.getBundle("com.dozor.langs.locales.Words", new Locale("ru", "RU"), UTF_8_CONTROL);
+        }
+        SELF = bundle;
+    }
+
 
     public static ResourceBundle getInstance() {
         return SELF;
