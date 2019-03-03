d2c9118b627266f484d043d06376571aa5e81bbc
Igor Kachkovsky <kachkovsky-noreply-spam@yandex.ru>
2019-03-03 14:24:09 +0400

    Try to fix read UTF-8 props for JDK 8

--- DozorClient/src/main/java/com/dozor/langs/LocaleBundle.java
+++ DozorClient/src/main/java/com/dozor/langs/LocaleBundle.java
@@ -1,17 +1,41 @@
 package com.dozor.langs;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 /**
  * @author IGOR-K
  */
 public class LocaleBundle {
 
-    private final static ResourceBundle SELF
-            = ResourceBundle.getBundle("com.dozor.langs.locales.Words", new Locale("ru", "RU"));
+    private static class InstanceHolder {
+        public static LocaleBundle INSTANCE = new LocaleBundle();
+    }
 
-    public static ResourceBundle getInstance() {
-        return SELF;
+    public static LocaleBundle getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private Properties props = new Properties();
+
+    private LocaleBundle() {
+        try {
+            InputStream is = getClass().getResourceAsStream("/com/dozor/langs/locales/Words_ru_RU.properties");
+            InputStreamReader stream = new InputStreamReader(is, StandardCharsets.UTF_8);
+            props.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getString(String key) {
+        return props.getProperty(key);
+    }
+
+    public boolean containsKey(String key) {
+        return props.containsKey(key);
     }
 }
